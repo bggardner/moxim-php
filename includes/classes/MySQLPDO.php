726b109be6f7fd80373e53c8681c2040bacaff95
Brent Gardner <brent@ebrent.net>
2012-04-16 18:01:39 -0400

    first commit

---
+++ includes/classes/MySQLPDO.php
@@ -0,0 +1,44 @@
+<?php
+  namespace MoXIM;
+  
+  include '../../../classes/MySQLPDO.php';
+  
+  /* This class is extended by BaseService, which performs error checking and injection prevention. */
+  
+  class MySQLService extends PDOService
+  {    
+    private function _connect($dbname)
+    {
+      // Connect to database using PDO
+      //return new PDO('mysql:host='.$host.';username='.$username.';password='.$password.';dbname='.$dbname);
+      return new \eBrent\MySQLPDO($dbname);
+    }
+    
+    protected function addModule($name)
+    {
+      $sql = 'INSERT INTO `modules` (name) VALUES ('.$name.')';
+      $this->pdo->exec($sql);
+      return (int) $this->pdo->lastInsertId();
+    }
+    
+    protected function addRelation($domain, $name, $range)
+    {
+      $sql = 'INSERT INTO `relations` (`domain`, `name`, `range`) VALUES ('.$id.', "'.$name.'", '.$range.')';
+      $this->pdo->exec($sql);
+      return (int) $this->pdo->lastInsertId();
+    }
+    
+    protected function deleteModule($id)
+    {
+      $sql = 'DELETE FROM `modules` WHERE id = '.$id;
+      return (bool) $this->pdo->exec($sql);
+    }
+    
+    protected function idExists($table, $id)
+    {
+      $sql = 'SELECT 1 FROM `'.$table.'` WHERE id = '.$id;
+      return (bool) $this->pdo->exec($sql);
+    }
+  }
+  
+?>
